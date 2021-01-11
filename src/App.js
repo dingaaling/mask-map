52c9dbc077f3f4943dc321c314ba80adf9ac377e
marco <marco.berlot@gmail.com>
2021-01-10 19:34:36 -0500

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Grid from '@material-ui/core/Grid';
-import {  Map, TileLayer, Marker } from 'react-leaflet'
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
 
+//Firebase
 import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import firebaseConfig from './config.js'
 
+//Internal Classes
+import Emojis from './Emojis.js';
+import HeatMap from './HeatMap.js'
+import PathMap from './PathMap.js'
+
+//Styling
 import './App.css'
-import { iconGlasses, iconMaskhole, iconNoMask } from './icon';
-import Emoji from './Emoji.js';
-import glasses from './images/glasses.png'
-import no_mask from './images/no_mask_red.png'
-import maskhole from './images/maskhole_red.png'
-import { nycdata } from './2020_maskhole_data';
 
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -55,6 +52,8 @@ class App extends React.Component {
       case error.UNKNOWN_ERROR:
         alert("An unknown error occurred.")
         break;
+      default:
+        alert("An unknown error occurred.")
     }
   }
 
@@ -70,13 +69,14 @@ class App extends React.Component {
     this.getLocation()
   };
 
+  isUserLggedIn(user) {
+    return user != null;
+  }
 
   imageClick(maskStatus) {
-
       let current_this = this
       if(navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
-
               // log to database
               let body = {
                 timestamp: new Date().toUTCString(),
@@ -86,189 +86,83 @@ class App extends React.Component {
                 accuracy: position.coords.accuracy,
               };
 
-              firebaseApp.database().ref('/').push(body);
+              //firebaseApp.database().ref('/').push(body);
               console.log("Data Saved");
 
-              if(maskStatus==0) {
-                current_this.setState({mask_list: current_this.state.mask_list.concat([[position.coords.latitude, position.coords.longitude]])});
-              }
-              if(maskStatus==1) {
-                current_this.setState({maskhole_list: current_this.state.maskhole_list.concat([[position.coords.latitude, position.coords.longitude]])});
-              }
-              if(maskStatus==2) {
-                current_this.setState({nomask_list: current_this.state.nomask_list.concat([[position.coords.latitude, position.coords.longitude]])});
-              }
-
-              current_this.setState({map_center: [position.coords.latitude, position.coords.longitude]});
-
-
-          });
-      } else {
-          alert("Geolocation error - please refresh page.");
-      }
+              switch (maskStatus) {
+                case 0:
+                  current_this.setState({mask_list: current_this.state.mask_list.concat([[position.coords.latitude, position.coords.longitude]])});
+                  break;
+                case 1:
+                  current_this.setState({maskhole_list: current_this.state.maskhole_list.concat([[position.coords.latitude, position.coords.longitude]])});
+                  break;
+                case 2:
+                  current_this.setState({nomask_list: current_this.state.nomask_list.concat([[position.coords.latitude, position.coords.longitude]])});
+                  break;
+                default:
+                  current_this.setState({map_center: [position.coords.latitude, position.coords.longitude]});
+              };
+    });
+  } else {
+      alert("Geolocation error - please refresh page.");
   }
+}
 
-render(){
+getLineSeparator() {
+  return <br></br>;
+}
 
+getFooter(props) {
   const {
     user,
     signOut,
     signInWithGoogle,
-  } = this.props;
+  } = props;
+  return(
+    <footer>
+      <center>
+      {
+        user
+          ? <button onClick={signOut}>SIGN OUT</button>
+          : <button onClick={signInWithGoogle}>SIGN IN</button>
+      }
+      </center>
+    </footer>
+)}
 
-  if(user){
+render(){
 
-    return (
+  return (
+    <div className="App">
 
+      <header className="App-header">
+        <h1><center>MASK MAP</center></h1>
+      </header>
+      {this.getLineSeparator()}
 
-      <div className="App">
-
-        <header className="App-header">
-          <h1><center>MASK MAP</center></h1>
-        </header>
-
-        <br></br>
-
-        <Grid container item xs={12} spacing={1}>
-
-            <Emoji img={glasses} function = {() => this.imageClick(0)}></Emoji>
-            <Emoji img={maskhole} function = {() => this.imageClick(1)}></Emoji>
-            <Emoji img={no_mask} function = {() => this.imageClick(2)}></Emoji>
-
-
-        </Grid>
-
-        <Grid container item xs={12} spacing={1}>
-          <Grid item xs={4}>
-            <p><center>{this.state.mask_list.length}</center></p>
-          </Grid>
-          <Grid item xs={4}>
-            <p><center>{this.state.maskhole_list.length}</center></p>
-          </Grid>
-          <Grid item xs={4}>
-            <p><center>{this.state.nomask_list.length}</center></p>
-          </Grid>
-
-        </Grid>
-
-        <div className="leaflet-container">
-
-          <Grid container item xs={12} spacing={1}>
-            <Grid item xs={2}></Grid>
-            <Grid item xs={8}>
-                  <Map center={this.state.map_center} zoom={15}>
-                  <TileLayer
-                    attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                  />
-
-                  {this.state.mask_list.map((position, idx) =>
-                    <Marker key={`marker-${idx}`} position={position} icon={ iconGlasses }>
-                    </Marker>
-                  )}
-
-                  {this.state.maskhole_list.map((position, idx) =>
-                    <Marker key={`marker-${idx}`} position={position} icon={ iconMaskhole }>
-                    </Marker>
-                  )}
-
-                  {this.state.nomask_list.map((position, idx) =>
-                    <Marker key={`marker-${idx}`} position={position} icon={ iconNoMask }>
-                    </Marker>
-                  )}
+      <Emojis onClick = {() => this.imageClick()}
+        mask_list = {this.state.mask_list}
+        maskhole_list = {this.state.maskhole_list}
+        nomask_list = {this.state.nomask_list}
+        is_user_logged_in = {this.isUserLggedIn()}>
+      </Emojis>
 
 
-                  </Map>
-            </Grid>
-            <Grid item xs={2}></Grid>
+      {this.isUserLggedIn() && <PathMap map_center = {this.state.map_center}
+        mask_list = {this.state.mask_list}
+        maskhole_list = {this.state.maskhole_list}
+        nomask_list = {this.state.nomask_list}/>
+      }
 
-          </Grid>
-          </div>
+      {!this.isUserLggedIn() && <HeatMap/>}
 
-        <br></br>
+      {this.getLineSeparator()}
 
-        <footer>
-          <center>
-          {
-            user
-              ? <button onClick={signOut}>SIGN OUT</button>
-              : <button onClick={signInWithGoogle}>SIGN IN</button>
-          }
-          </center>
-        </footer>
+     {this.getFooter(this.props)}
 
-      </div>
-
-
+     </div>
     );
   }
-
-  else{
-    return(
-      <div className="App">
-
-        <header className="App-header">
-          <h1><center>MASK MAP NYC</center></h1>
-        </header>
-
-        <br></br>
-
-        <Grid container item xs={12} spacing={1}>
-
-            <Emoji img={glasses}></Emoji>
-            <Emoji img={maskhole} ></Emoji>
-            <Emoji img={no_mask}></Emoji>
-
-        </Grid>
-
-        <br></br>
-
-        <div className="leaflet-container">
-
-          <Grid container item xs={12} spacing={1}>
-            <Grid item xs={2}></Grid>
-            <Grid item xs={8}>
-                  <Map center={[40.760186, -73.972084]} zoom={12}>
-                  <HeatmapLayer
-                    fitBoundsOnLoad
-                    fitBoundsOnUpdate
-                    max={1.0}
-                    radius={15}
-                    points={nycdata}
-                    longitudeExtractor={m => m[1]}
-                    latitudeExtractor={m => m[0]}
-                    intensityExtractor={m => parseFloat(m[2])} />
-                  <TileLayer
-                    attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                  />
-                  </Map>
-            </Grid>
-            <Grid item xs={2}></Grid>
-
-          </Grid>
-          </div>
-
-        <footer>
-          <center>
-          {
-            user
-              ? <h3> </h3>
-              : <h3>PLEASE SIGN IN TO LOG DATA</h3>
-          }
-          {
-            user
-              ? <button onClick={signOut}>SIGN OUT</button>
-              : <button onClick={signInWithGoogle}>SIGN IN</button>
-          }
-          </center>
-        </footer>
-
-      </div>
-    );
-  }
-}
-
 }
 
 export default withFirebaseAuth({
---
+++ src/HeatMap.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import {  Map, TileLayer } from 'react-leaflet'
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+
+
+
+
+class HeatMap extends React.Component {
+
+render(){
+    return (
+      <div className="leaflet-container">
+
+        <Grid container item xs={12} spacing={1}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8}>
+                <Map center={[40.760186, -73.972084]} zoom={12}>
+                <HeatmapLayer
+                  fitBoundsOnLoad
+                  fitBoundsOnUpdate
+                  max={1.0}
+                  radius={15}
+                  longitudeExtractor={m => m[1]}
+                  latitudeExtractor={m => m[0]}
+                  intensityExtractor={m => parseFloat(m[2])} />
+                <TileLayer
+                  attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+                </Map>
+          </Grid>
+          <Grid item xs={2}></Grid>
+
+        </Grid>
+        </div>
+    );
+
+}
+
+}
+
+export default HeatMap;
